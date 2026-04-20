9fc5317801d2d04a4a6d687d1d5de2bf512dd603
JetCemetery <59208171+JetCemetery@users.noreply.github.com>
2026-04-20 18:29:09 -0400

    added JD history saving, need to make button more dynamic in size

--- ATS_Injector/Form1.cs
+++ ATS_Injector/Form1.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static ATS_Injector.Helper;
 using static ATS_Injector.PopOutApp;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
@@ -272,85 +273,115 @@ public partial class Form1 : Form
     private void ProcessCreateAction_btn_Click(object sender, EventArgs e)
     {
         ATS_Injection_txt.Text = string.Empty;
-        Task.Run(() =>
+        bool continueOp = true;
+        if (History.NearMatchJDFound(ManualJDPaste_txt.Text))
         {
-            string Token = string.Empty;
-            string errorMsg;
-            string result = string.Empty;
-            API_AI_ID choice = GetSelectedAPI();
-            switch (choice)
+            continueOp = false;
+            using (DuplicateMatchForm popup = new Helper.DuplicateMatchForm(History.GetNearestMatch(ManualJDPaste_txt.Text), History.cleanDirtyData(ManualJDPaste_txt.Text)))
             {
-                case API_AI_ID.ChatGPT:
-                    break;
-                case API_AI_ID.Gemini:
+                DialogResult result = popup.ShowDialog();
 
-                    //When this function is called, it shall do one of two things
-                    //1 - Process the Job Description and send it to AI
-                    if (GetAPI_Token(API_AI_ID.Gemini, out Token, out errorMsg))
-                    {
-                        //at least we know token is written, pass it to the API thing now
-                        GeminiAPI API = new GeminiAPI(Token);
-                        //ProcessJD_btn.Enabled = false;
-                        //string prompt = "Explain DO-178C compliance levels.";
-                        string promptTry1 = Helper.AI_ATS_Question;
-                        Helper.FeedBackHelper.AppendFeedback($"Using {API_AI_ID.Gemini} AI to generate ATS friendly keywords and phrases.\r\n");
+                if (result == DialogResult.OK)
+                {
+                    // User clicked OK - Proceed with logic using commonEntries
+                    int abc = 4;
 
-                        string prompt = $"{promptTry1}\r\n{Helper.FeedBackHelper.GetTextManualJDPaste()}";
-
-                        // This runs the task on a ThreadPool thread and waits for the result
-                        result = Task.Run(async () => await API.SendPrompt(prompt))
-                                            .GetAwaiter()
-                                            .GetResult();
-                        //JD_Results = result;
-
-                    }
-                    else if (string.IsNullOrEmpty(errorMsg) == false)
-                    {
-                        FeedbackArea_txt.Text = $"An error occured retrieving your {API_AI_ID.Gemini} API token. Error stack:[{errorMsg}]";
-                    }
-
-                    break;
-                case API_AI_ID.Claude:
-
-                    if (GetAPI_Token(API_AI_ID.Claude, out Token, out errorMsg))
-                    {
-                        OpenRouter_Calude API = new OpenRouter_Calude(Token);
-                        //ProcessJD_btn.Enabled = false;
-                        //string prompt = "Explain DO-178C compliance levels.";
-                        string promptTry1 = Helper.AI_ATS_Question;
-                        Helper.FeedBackHelper.AppendFeedback($"Using {API_AI_ID.Claude} AI to generate ATS friendly keywords and phrases.\r\n");
-
-                        string prompt = $"{promptTry1}\r\n{Helper.FeedBackHelper.GetTextManualJDPaste()}";
-
-                        // This runs the task on a ThreadPool thread and waits for the result
-                        result = Task.Run(async () => await API.SendPrompt(prompt))
-                                            .GetAwaiter()
-                                            .GetResult();
-
-                        int ab = 4;
-                    }
-                    else
-                    {
-
-                    }
-                    break;
-                case API_AI_ID.NO_TOKEN:
-                    break;
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    // User clicked Cancel or closed the window
+                    Console.WriteLine("User cancelled the operation. No changes made.");
+                }
             }
-
-
-            if (result.Equals(GeminiAPI.GeminiTimeOut))
-            {
                 //TODO
-                //add a pop up asking if they want to try again
-            }
-            Helper.FeedBackHelper.SetATS_Injection(result);
-            Update_ProcessCreateAction_btn();
-            Helper.FeedBackHelper.AppendFeedback($"Does the ATS injection look Acceptable at the bottom? \r\n");
-            ProgressBar(ProgressBarStat.AI_START);
-        });
+                //window pop up I guess or some sort of feedback?
+                //GetNearestMatch
+//                Helper.DuplicateMatchForm dupFound = new Helper.DuplicateMatchForm(History.GetNearestMatch(ManualJDPaste_txt.Text), History.cleanDirtyData(ManualJDPaste_txt.Text));
+            continueOp = true;
+        }
 
-        ProgressBar(ProgressBarStat.JD_START);
+        if (continueOp)
+        {
+            Task.Run(() =>
+            {
+                string Token = string.Empty;
+                string errorMsg;
+                string result = string.Empty;
+                API_AI_ID choice = GetSelectedAPI();
+                switch (choice)
+                {
+                    case API_AI_ID.ChatGPT:
+                        break;
+                    case API_AI_ID.Gemini:
+
+                        //When this function is called, it shall do one of two things
+                        //1 - Process the Job Description and send it to AI
+                        if (GetAPI_Token(API_AI_ID.Gemini, out Token, out errorMsg))
+                        {
+                            //at least we know token is written, pass it to the API thing now
+                            GeminiAPI API = new GeminiAPI(Token);
+                            string promptTry1 = Helper.AI_ATS_Question;
+                            Helper.FeedBackHelper.AppendFeedback($"Using {API_AI_ID.Gemini} AI to generate ATS friendly keywords and phrases.\r\n");
+
+                            string prompt = $"{promptTry1}\r\n{Helper.FeedBackHelper.GetTextManualJDPaste()}";
+
+                            // This runs the task on a ThreadPool thread and waits for the result
+                            result = Task.Run(async () => await API.SendPrompt(prompt))
+                                                .GetAwaiter()
+                                                .GetResult();
+                        }
+                        else if (string.IsNullOrEmpty(errorMsg) == false)
+                        {
+                            FeedbackArea_txt.Text = $"An error occured retrieving your {API_AI_ID.Gemini} API token. Error stack:[{errorMsg}]";
+                        }
+
+                        break;
+                    case API_AI_ID.Claude:
+
+                        if (GetAPI_Token(API_AI_ID.Claude, out Token, out errorMsg))
+                        {
+                            OpenRouter_Calude API = new OpenRouter_Calude(Token);
+                            //ProcessJD_btn.Enabled = false;
+                            //string prompt = "Explain DO-178C compliance levels.";
+                            string promptTry1 = Helper.AI_ATS_Question;
+                            Helper.FeedBackHelper.AppendFeedback($"Using {API_AI_ID.Claude} AI to generate ATS friendly keywords and phrases.\r\n");
+
+                            string prompt = $"{promptTry1}\r\n{Helper.FeedBackHelper.GetTextManualJDPaste()}";
+
+                            // This runs the task on a ThreadPool thread and waits for the result
+                            result = Task.Run(async () => await API.SendPrompt(prompt))
+                                                .GetAwaiter()
+                                                .GetResult();
+
+                            int ab = 4;
+                        }
+                        else
+                        {
+
+                        }
+                        break;
+                    case API_AI_ID.NO_TOKEN:
+                        break;
+                }
+
+
+                if (result.Equals(GeminiAPI.GeminiTimeOut))
+                {
+                    //TODO
+                    //add a pop up asking if they want to try again
+                }
+                Helper.FeedBackHelper.SetATS_Injection(result);
+                Update_ProcessCreateAction_btn();
+                Helper.FeedBackHelper.AppendFeedback($"Does the ATS injection look Acceptable at the bottom? \r\n");
+                ProgressBar(ProgressBarStat.AI_START);
+            });
+
+            ProgressBar(ProgressBarStat.JD_START);
+        }
+        else
+        {
+            //JD was a dup, probably...
+        }
     }
 
     private void ProgressBar(ProgressBarStat enumState)
@@ -514,6 +545,7 @@ public partial class Form1 : Form
 
         if (QC_Passed)
         {
+            History.SaveData(ManualJDPaste_txt.Text);
             PDFInjector.InjectHiddenText(infile, outFile, bulletPoints);
             ProgressBar(ProgressBarStat.END);
         }
